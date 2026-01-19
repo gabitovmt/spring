190ed6f8a13ab0bad0a28e2f805e606e3243e34e
Marat Gabitov <gabitovmt@yandex.ru>
2026-01-19 19:44:34 +0500

    Глава 2. Начало работы. Hello world

---
+++ prospring5/ch02/src/main/java/ru/maratgabitov/ch02/HelloWorld.java
@@ -0,0 +1,8 @@
+package ru.maratgabitov.ch02;
+
+public class HelloWorld {
+    @SuppressWarnings("java:S106")
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ prospring5/ch02/src/main/java/ru/maratgabitov/ch02/HelloWorldWithCommandLine.java
@@ -0,0 +1,12 @@
+package ru.maratgabitov.ch02;
+
+public class HelloWorldWithCommandLine {
+    @SuppressWarnings("java:S106")
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println(args[0]);
+        } else {
+            System.out.println("Hello World!");
+        }
+    }
+}
