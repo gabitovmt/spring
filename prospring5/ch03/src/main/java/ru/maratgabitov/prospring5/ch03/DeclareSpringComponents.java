46c8fdb7b3dc012b1147631c156c61eecd8058b4
Marat Gabitov <gabitovmt@yandex.ru>
2026-01-23 14:08:28 +0500

    Глава 03. Объявление компонентов Spring

---
+++ prospring5/ch03/src/main/java/ru/maratgabitov/prospring5/ch03/DeclareSpringComponents.java
@@ -0,0 +1,15 @@
+package ru.maratgabitov.prospring5.ch03;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+import ru.maratgabitov.prospring5.ch02.decoupled.MessageRenderer;
+
+public class DeclareSpringComponents {
+    public static void main(String[] args) {
+        var ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:spring/app-context-annotation.xml");
+        ctx.refresh();
+        var messageRenderer = ctx.getBean("renderer", MessageRenderer.class);
+        messageRenderer.render();
+        ctx.close();
+    }
+}
---
+++ prospring5/ch03/src/main/java/ru/maratgabitov/prospring5/ch03/annotation/HelloWorldMessageProvider.java
@@ -0,0 +1,13 @@
+package ru.maratgabitov.prospring5.ch03.annotation;
+
+import org.springframework.stereotype.Component;
+import ru.maratgabitov.prospring5.ch02.decoupled.MessageProvider;
+
+// Простой компонент Spring Bean
+@Component("provider")
+public class HelloWorldMessageProvider implements MessageProvider {
+    @Override
+    public String getMessage() {
+        return "Hello World!";
+    }
+}
