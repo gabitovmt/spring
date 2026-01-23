549447ca986cd8de2166b58d43d093985ef10eff
Marat Gabitov <gabitovmt@yandex.ru>
2026-01-23 19:12:40 +0500

    Глава 03. Внедрение через конструктор

---
+++ prospring5/src/main/java/ru/maratgabitov/prospring5/ch03/confused/App1.java
@@ -0,0 +1,13 @@
+package ru.maratgabitov.prospring5.ch03.confused;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class App1 {
+
+    public static void main(String[] args) {
+        var ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:spring/ch03/constructor-confusion.xml");
+        ctx.refresh();
+        ctx.close();
+    }
+}
