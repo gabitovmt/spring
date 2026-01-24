375957656838c5d38f15642afa2b11966548c8e4
Marat Gabitov <gabitovmt@yandex.ru>
2026-01-24 16:24:05 +0500

    Глава 03. Внедрение и вложение контекстов приложений

---
+++ prospring5/src/main/java/ru/maratgabitov/prospring5/ch03/hierarchical/Song.java
@@ -0,0 +1,13 @@
+package ru.maratgabitov.prospring5.ch03.hierarchical;
+
+public class Song {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
