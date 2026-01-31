fbd4cef95b7cde3bfb4ffe17bae6507895ffbf50
Marat Gabitov <gabitovmt@yandex.ru>
2026-01-31 13:13:33 +0500

    Глава 04. Описание порядка разрешения зависимостей

---
+++ prospring5/src/main/java/ru/maratgabitov/prospring5/ch04/shutdownhook/DestructiveBeanWithHook.java
@@ -0,0 +1,34 @@
+package ru.maratgabitov.prospring5.ch04.shutdownhook;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.maratgabitov.prospring5.ch04.destroybean.DestructiveBean;
+
+import java.nio.file.FileSystems;
+
+public class DestructiveBeanWithHook {
+
+    @Configuration
+    static class DestructiveBeanConfig {
+
+        @Bean(initMethod = "afterPropertiesSet", destroyMethod = "destroy")
+        DestructiveBean destructiveBean() {
+            var bean = new DestructiveBean();
+            bean.setFilePath(
+                    System.getProperty("java.io.tmpdir")
+                            + FileSystems.getDefault().getSeparator()
+                            + "test.txt"
+            );
+
+            return bean;
+        }
+    }
+
+    @SuppressWarnings("java:S106")
+    public static void main(String[] args) {
+        var ctx = new AnnotationConfigApplicationContext(DestructiveBeanConfig.class);
+        ctx.getBean(DestructiveBean.class);
+        ctx.registerShutdownHook();
+    }
+}
