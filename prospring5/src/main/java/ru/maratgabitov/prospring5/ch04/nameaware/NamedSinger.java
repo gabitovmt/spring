dcc24b85fa406fd1990ef7c1542875d12c938f01
Marat Gabitov <gabitovmt@yandex.ru>
2026-02-03 14:41:26 +0500

    Глава 04. Информирование компонентов об их контексте

---
+++ prospring5/src/main/java/ru/maratgabitov/prospring5/ch04/nameaware/NamedSinger.java
@@ -0,0 +1,19 @@
+package ru.maratgabitov.prospring5.ch04.nameaware;
+
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.stereotype.Component;
+
+@Component("johnMayer")
+public class NamedSinger implements BeanNameAware {
+    private String name;
+
+    @Override
+    public void setBeanName(String name) {
+        this.name = name;
+    }
+
+    @SuppressWarnings("java:S106")
+    public void sing() {
+        System.out.println("Singer " + name + " - sing()");
+    }
+}
