3b09f4ba4feab585672f0a1969d155051de8fa5b
Marat Gabitov <gabitovmt@yandex.ru>
2026-04-25 20:09:07 +0500

    Shell. Command registration

--- spring-shell/shell/src/main/java/spring/commandregistration/App.java
+++ spring-shell/shell/src/main/java/spring/commandregistration/App.java
@@ -1,13 +1,15 @@
 package spring.commandregistration;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.shell.core.ShellRunner;
 import org.springframework.shell.core.command.annotation.EnableCommand;
 import spring.support.ShellRunnerConfig;
 
 @Import(ShellRunnerConfig.class)
-@EnableCommand
+@ComponentScan
+@EnableCommand(MyCommands.class)
 public class App {
 
     public static void main(String[] args) throws Exception {
