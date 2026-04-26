bba812e3ddb53cfc222326e09de654c6819aa540
Marat Gabitov <gabitovmt@yandex.ru>
2026-04-26 11:35:56 +0500

    Shell. Command registration

--- spring-shell/shell/src/main/java/spring/commandregistration/App.java
+++ spring-shell/shell/src/main/java/spring/commandregistration/App.java
@@ -1,9 +1,14 @@
 package spring.commandregistration;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.shell.core.ShellRunner;
+import org.springframework.shell.core.command.AbstractCommand;
+import org.springframework.shell.core.command.Command;
+import org.springframework.shell.core.command.CommandContext;
+import org.springframework.shell.core.command.ExitStatus;
 import org.springframework.shell.core.command.annotation.EnableCommand;
 import spring.support.ShellRunnerConfig;
 
@@ -16,5 +21,23 @@ public class App {
         var ctx = new AnnotationConfigApplicationContext(App.class);
         var runner = ctx.getBean(ShellRunner.class);
         runner.run(args);
+    }
+
+    @Bean
+    Command foo() {
+        return Command.builder().name("foo").execute(context -> {
+            context.outputWriter().println("FOO");
+        });
+    }
+
+    @Bean
+    Command bar() {
+        return new AbstractCommand("bar", "This is bar command") {
+            @Override
+            public ExitStatus doExecute(CommandContext commandContext) {
+                println("BAR", commandContext);
+                return ExitStatus.OK;
+            }
+        };
     }
 }
